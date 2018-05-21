890715da578c4871c7c51b55025d24ac71c203a1
DrakonLisowski <dragon.fox.66@yandex.ru>
2018-05-21 11:48:39 +0300

    Тест команды strike

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const botgram = require("botgram")
 const bot = botgram(process.env.TELEGRAM_BOT_TOKEN)
 
+bot.command("strike", (msg, reply) =>
+    reply.text(msg)
+)
+
 bot.command((msg, reply) =>
     reply.text("Invalid command."))
