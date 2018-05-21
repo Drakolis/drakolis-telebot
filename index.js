ec5e40282d3f034d077ee8cef22771369cbe3909
DrakonLisowski <dragon.fox.66@yandex.ru>
2018-05-21 11:54:11 +0300

    Алгоритм зачеркивания

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const botgram = require("botgram")
 const bot = botgram(process.env.TELEGRAM_BOT_TOKEN)
 
-bot.command("strike", (msg, reply) =>
-    reply.text(msg)
-)
+bot.command("strike", (msg, reply) => {
+    var text = msg.args(1);
+    var replyText = "";
+    text.forEach(element => {
+        replyText += element + "̶";
+    });
+    reply.text(text)
+})
 
 bot.command((msg, reply) =>
     reply.text("Invalid command."))
