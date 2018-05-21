2272f00f769997559b84fb8bdc7ed33dfe39d053
DrakonLisowski <dragon.fox.66@yandex.ru>
2018-05-21 12:28:58 +0300

    Добавлен код сервера для выживаемости

--- index.js
+++ index.js
@@ -34,3 +34,9 @@ bot.command("roll", (msg, reply) => {
 bot.command((msg, reply) =>
     reply.text("Не знаю никаких '" + msg.command + "'.")
 )
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function () {
+    console.log('Listening on port %d', server_port);
+});
